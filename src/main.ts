b204c15bd87865b702fc8c505e08d06bd9b49549
JaviFiallos <efiallos2433@uta.edu.ec>
2025-10-30 21:33:50 -0500

    Cambie el puerto al orignal

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
