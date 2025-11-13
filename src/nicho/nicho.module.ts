acd964e81c8847b57b1309e9713efdeb8fb4fc05
tichekiwar <kiwartiche@gmail.com>
2025-11-13 18:15:00 -0500

    feat: asignacion automatica de nichos a bloques

--- src/nicho/nicho.module.ts
+++ src/nicho/nicho.module.ts
@@ -8,12 +8,13 @@ import { Nicho } from './entities/nicho.entity';
 import { HuecosNicho } from 'src/huecos-nichos/entities/huecos-nicho.entity';
 import { Persona } from 'src/personas/entities/persona.entity';
 import { PropietarioNicho } from 'src/propietarios-nichos/entities/propietarios-nicho.entity';
+import { Bloque } from 'src/bloques/entities/bloque.entity';
 import { PaymentModule } from 'src/payment/payment.module';
 import { PropietariosNichosModule } from 'src/propietarios-nichos/propietarios-nichos.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Nicho, HuecosNicho, Persona, PropietarioNicho]),
+    TypeOrmModule.forFeature([Nicho, HuecosNicho, Persona, PropietarioNicho, Bloque]),
     PaymentModule,
     PropietariosNichosModule,
   ],
